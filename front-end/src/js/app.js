5132184230fcffcbf787e7454ff9f55a1857c7fa
Facundo E. Beltramo <fexbef@gmail.com>
2015-04-01 12:54:47 -0300

    adds

--- front-end/src/js/app.js
+++ front-end/src/js/app.js
@@ -6,6 +6,7 @@
          "ui.router",
          "ui.bootstrap",
          "ngFx",
+         "ngResource",
          "chieffancypants.loadingBar"
       ])
       .config(function(cfpLoadingBarProvider) {
--- front-end/src/js/ctrl.login.js
+++ front-end/src/js/ctrl.login.js
@@ -2,9 +2,19 @@
 (function(){
 
    angular.module('main')
-      .controller('ctrl.login', function($timeout){
+      .controller('ctrl.login', function($timeout, $resource){
         var vm = this;
          vm.animation='';
+
+         vm.do = function() {
+            $resource('/login').save({
+               username: vm.username,
+               password: vm.password
+            }, function(resopnse){
+            
+            });
+         };
+
 
          $timeout(function(){
             vm.animation='fx-fade-up fx-speed-600';
