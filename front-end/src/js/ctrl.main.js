3a4206aba52ef0308f052c03adde1c2e1492132e
Facundo E. Beltramo <fexbef@gmail.com>
2015-04-03 06:01:51 -0300

    [add] style to main menu.

--- front-end/src/js/ctrl.main.js
+++ front-end/src/js/ctrl.main.js
@@ -2,8 +2,18 @@
 (function(){
 
    angular.module('main')
-      .controller('ctrl.main', function($timeout, $state, cfpLoadingBar){
-         this.state = $state;
+      .controller('ctrl.main', function($window, $timeout, $state, cfpLoadingBar){
+         var vm = this;
+
+         vm.state = $state;
+         
+         vm.nextpage = function(){
+            $window.history.forward();
+         };
+         vm.backpage = function(){
+            $window.history.back();
+         };
+
          cfpLoadingBar.start();
       });
 
